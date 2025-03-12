26ea68fb8776b22d0cb5b77d76e9266160b9a7e0
Dhruvi Mevada <dhruvimn19@gmail.com>
2025-03-12 14:40:07 +0530

    Initial commit

---
+++ Backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
+
+//connect database
+mongoose.connect("mongodb://127.0.0.1:27017/time-credit-platform",)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//connect API
+const userRoutes = require("./Routes/userRoutes");
+app.use("/users", userRoutes);
+
+const taskRoutes = require("./Routes/taskRoutes");
+app.use("/tasks", taskRoutes);
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = 7000;
+
+app.use(express.static(path.join(__dirname, "..", "Frontend")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "Frontend", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
